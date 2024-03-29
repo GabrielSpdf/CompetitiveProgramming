0381db7da56d17e139c9d06570af3005865784ea
GabrielSpdf <gb.spada@hotmail.com>
2024-03-29 03:18:58 -0300

    new exercs

---
+++ CodeForces/CF-Round928-A.cpp
@@ -0,0 +1,29 @@
+//Accepted
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	ios_base::sync_with_stdio();
+	cin.tie(0);
+
+	ll n;
+	cin >> n;
+	while(n--){
+		string s;
+		ll ver1=0, ver2=0;
+		cin >> s;
+
+		for(int i=0; i<(int)s.size(); i++){
+			if(s[i]=='A') ver1++;
+			else{ ver2++; }
+		}
+
+		cout << (ver1 > ver2? "A" : "B") << endl;
+	}
+
+	return 0; 
+}
