fe10e9bc5aef2339d1489ee0c936a77de97ff5c9
GabrielSpdf <gb.spada@hotmail.com>
2024-12-31 19:46:09 -0300

    updating base.cpp

--- base.cpp
+++ base.cpp
@@ -2,20 +2,34 @@
 
 using namespace std;
 
-const int INF = 1e9;
-const long long LLINF = 4e18;
-const double EPS = 1e-9;
-
 typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void solve(){
+
+
+}
 
 int main(){
+	/* setIO("problemname"); */
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
+	
+	/* int t; cin >> t; while(t--) */
+		solve();
 
     return 0;
 }
