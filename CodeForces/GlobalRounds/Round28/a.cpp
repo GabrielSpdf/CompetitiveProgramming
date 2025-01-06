c4a4269c700e83b96b9d27ba220e3a198c720292
GabrielSpdf <gb.spada@hotmail.com>
2025-01-06 02:00:13 -0300

    updating

---
+++ CodeForces/GlobalRounds/Round28/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void solve(){
+	int n;
+	cin >> n;
+	
+	if(n%33==0) cout << "YES\n";
+	else cout << "NO\n";
+}
+
+int main(){
+	/* setIO("problemname"); */
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	
+	int t; cin >> t; while(t--)
+		solve();
+
+    return 0;
+}
