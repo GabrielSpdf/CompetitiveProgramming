386b4dbd3e9383c0aa8e239c4220f36659d4060f
GabrielSpdf <gb.spada@hotmail.com>
2025-01-08 18:39:35 -0300

    updating create_contest?

--- create_contest.sh
+++ create_contest.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
 
 create_files(){
-	local base_dir="$1"
-	local contest_dir="$2"
-	local end_char="$3"
-	local template_file="$4"
-	local template_name="$5"
+	local current_dir=$(pwd)
+	local contest_dir="$1"
+	local end_char="$2"
+	local template_file="$3"
+	local template_name="$4"
 
-	local full_path="$base_dir/$contest_dir"
+	local full_path="${current_dir}/${contest_dir}"
 
 	if [ -d "$full_path" ]; then
-		echo "Error: Directory '$full_path' already exists."
-		exit 1
+		echo "Warning: Directory '$full_path' already exists."
+	else
+		mkdir -p "$full_path"
 	fi
 
-	mkdir -p "$full_path"
-
 	for char in a b c d e f g h i j k l m n o p; do
-	  if [[ "$char" > "$end_char" ]]; then
-		break
-	  fi
-	  cp "$template_file" "$full_path/$char.cpp"
-	  echo "Created $full_path/$char.cpp using $template_name"
+		if [[ "$char" > "$end_char" ]]; then
+			break
+		fi
+
+		local exerc_path="$full_path/$char.cpp"
+		if [ -f "$exerc_path" ]; then
+			echo "Warning: Directory '$exerc_path' already exists. Skipping to the next exerc..."
+		else	
+			cp "$template_file" "$exerc_path"
+			echo "Created $exerc_path using $template_name"
+		fi
 	done
+
 	cp "$template_file" "$full_path/base.cpp"
 	echo "Created $full_path/base.cpp using $template_name"
 }
 
 create_single_file() {
-	local base_dir="$1"
-	local contest_dir="$2"
-	local exercise_name="$3"
-	local template_file="$4"
-	local template_name="$5"
+	local current_dir=$(pwd)
+	local contest_dir="$1"
+	local exercise_name="$2"
+	local template_file="$3"
+	local template_name="$4"
 
-	local full_path="$base_dir/$contest_dir"
+	local full_path="${current_dir}/${contest_dir}"
 
 	if [ -d "$full_path" ]; then
-		echo "Error: Directory '$full_path' already exists."
-        exit 1
+		echo "Warning: Directory '$full_path' already exists."
+	else
+		mkdir -p "$full_path"
     fi
-  
-	mkdir -p "$full_path"
-	cp "$template_file" "$full_path/$exercise_name.cpp"
-	echo "Created $full_path/$exercise_name.cpp using $template_name"
+
+	local exerc_path="$full_path/$exercise_name.cpp"
+	if [ -f "$exerc_path" ]; then
+		echo "Error: Directory '$exerc_path' already exists."
+	else	
+		cp "$template_file" "$exerc_path"
+		echo "Created $exerc_path using $template_name"
+	fi
 }
 
 while [[ $# -gt 0 ]]; do
@@ -68,20 +79,20 @@ while [[ $# -gt 0 ]]; do
 			echo "###############################"
 			echo "#############CSES##############"
 			echo "###############################"
-            exercise_name="$4"
+            exercise_name="$3"
             TEMPLATE_FILE="./templates/cses_base.cpp"
             TEMPLATE_NAME="cses_base.cpp"
-            create_single_file "$2" "$3" "$exercise_name" "$TEMPLATE_FILE" "$TEMPLATE_NAME"
+            create_single_file "$2" "$exercise_name" "$TEMPLATE_FILE" "$TEMPLATE_NAME"
             exit 0
             ;;
         --usaco) # USACO
 			echo "###############################"
 			echo "#############USACO#############"
 			echo "###############################"
-			exercise_name="$4"
+			exercise_name="$3"
 			TEMPLATE_FILE="./templates/usaco_base.cpp"
 			TEMPLATE_NAME="usaco_base.cpp"
-			create_single_file "$2" "$3" "$exercise_name" "$TEMPLATE_FILE" "$TEMPLATE_NAME"
+			create_single_file "$2" "$exercise_name" "$TEMPLATE_FILE" "$TEMPLATE_NAME"
 			exit 0
 			;;
         -* | --*) # Error if there is not anything
@@ -98,11 +109,10 @@ if [ $# -lt 2 ] ; then
      echo "Usage: $0 [--cf|--atcoder] <base_dir> <contest_dir> <last_question_in_char (optional)>"
     exit 1
 fi
-base_dir="$1"
-contest_dir="$2"
-end_char="${3:-d}"
+contest_dir="$1"
+end_char="${2:-d}"
 
 # Call create_files with the extracted template and params
-create_files "$base_dir" "$contest_dir" "$end_char"
+create_files "$contest_dir" "$end_char"
 
 
