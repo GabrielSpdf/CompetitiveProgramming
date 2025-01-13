85e5103d5696d64a8917c9b8d356cc4341cafd8d
GabrielSpdf <gb.spada@hotmail.com>
2025-01-13 00:13:49 -0300

    updating

--- AtCoder/beg388/e.cpp
+++ AtCoder/beg388/e.cpp
@@ -1,5 +1,3 @@
-// Unsolved yet
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -20,31 +18,21 @@ void solve(){
 	cin >> n;
 	
 	vll v(n);
-	
 	for(auto &a: v) cin >> a;
 
-	ll ans=0;
-	ll i=v.size()-1, j=v.size()-1;
-	vector<bool> used(n, false);
-	while(i>=0 && j>=0){
-		cerr << "v[i]: " << v[i] << " ";
-		cerr << "v[j]: " << v[j] << " ";
-		cerr << "v[i]/2: " << v[i]/2 << "\n";
-		if(used[i]) i--;
-		else if(used[j]) j--;
-		else{
-			if(v[j]<=v[i]/2){
-				cerr << v[i] << " with " << v[j] << "\n";
-				used[i]=true;
-				used[j]=true;
-				ans++;
-				i--;
-				j--;
-			}
-			else{
-				j--;
-			}
+	vll v1, v2;
+	for(int i=0; i<n/2; i++) v1.push_back(v[i]);
+	for(int i=n/2; i<n; i++) v2.push_back(v[i]);
+
+	int i=0, j=0, ans=0;
+	while(i<(int)v1.size() && j<(int)v2.size()){
+		cerr << "v1[i]: " << v1[i] << " ";
+		cerr << "v2[j]: " << v2[j] << "\n";
+		if(v1[i]<=v2[j]/2LL){
+			ans++;
+			i++; j++;
 		}
+		else j++;
 	}
 
 	cout << ans << "\n";
--- CodeForces/EducationalRounds/Round112/b.cpp
+++ CodeForces/EducationalRounds/Round112/b.cpp
@@ -1,6 +1,5 @@
-// Unsolved yet
-
 #include <bits/stdc++.h>
+#include <climits>
 
 using namespace std;
 
@@ -22,79 +21,32 @@ struct Rec{
 	}
 };
 
-ll intersect(Rec p, Rec q){
-	ll widht = max(min(p.x2, q.x2)-min(p.x1, q.x1), 0LL);
-	ll length = max(min(p.y2, q.y2)-min(p.y1, q.y1), 0LL);
-
-	return widht*length;
-}
-
-double dst(ll px1, ll py1, ll px2, ll py2){ 
-	return sqrtl(((px1-px2)*(px1-px2))+((py1-py2)*(py1-py2)));
-}
-
 void solve(){
-	ll rw, rh;
-	cin >> rw >> rh;
+	ll W, H;
+	cin >> W >> H;
+
 	Rec t;
 	cin >> t.x1 >> t.y1 >> t.x2 >> t.y2;
+
 	ll w, h;
 	cin >> w >> h;
 	
 	ll width = t.x2 - t.x1;
 	ll length = t.y2 - t.y1;
-	if(width + w > rw && length + h > rh){ cout << -1 << "\n"; return; }
+	if(width + w > W && length + h > H){ cout << -1 << "\n"; return; }
 
-	bool left=false, right=false, top=false, bottom=false;
-	if(abs(t.x1-0)<(rw-t.x2)){ // mais perto da esquerda
-		right=true;	
+	ll dist = LLONG_MAX;
+	if(width + w <= W){
+		dist = min(dist, max((w + t.x2 - W), 0LL));
+		dist = min(dist, max((w - t.x1), 0LL));
 	}
-	else left=true;
-	if(abs(t.y1-0)<(rh-t.y2)){ 
-		top=true;
+	if(length + h <= H){
+		dist = min(dist, max((h + t.y2 - H), 0LL));
+		dist = min(dist, max((h - t.y1), 0LL));
 	}
-	else bottom=true;
 	
-	Rec b;
-	if(left){
-		b.x1=0;
-		b.x2=0+w;
-	}
-	else if(right){
-		b.x1=rw-w;
-		b.x2=rw;
-	}
-
-	if(bottom){
-		b.y1=0;
-		b.y2=0+h;
-	}
-	else if(top){
-		b.y1=rh-h;
-		b.y2=rh;
-	}
-
-	if(!intersect(t, b)){ cout << 0 << "\n"; return; }
-
-	/* cerr << "top: " << top << "\n"; */
-	/* cerr << "left: " << left << "\n"; */
-	/* cerr << "right: " << right << "\n"; */
-	/* cerr << "bottom: " << bottom << "\n"; */
-	cerr << b.x1 << " " << b.y1 << " " << b.x2 << " " << b.y2 << "\n";
+	if(dist==LONG_LONG_MAX) dist=0;
 	
-	double dist;
-	if(left && bottom) dist = dst(b.x2, b.y2, t.x1, t.y1);
-	else if(left && top) dist = dst(b.x2, b.y1, t.x1, t.y2);
-	else if(right && bottom) dist =  dst(b.x1, b.y2, t.x2, t.y1);
-	else if(right && top) dist = dst(b.x1, b.y1, t.x2, t.y2);
-	
-	cerr << "w: " << w << " | width: " << width << " | rw: " << rw << "\n";
-	if(w+width<=rw){ dist = min(min(dist, max(double(b.x2-t.x1), 0.0)), max(double(t.x2-b.x1), 0.0)); cerr << "entrou1\n"; }
-	if(h+length<=rh){ dist = min(min(dist, max(double(b.y2-t.y1), 0.0)), max(double(t.y2-b.y1), 0.0)); cerr << "entrou2\n"; }
-	
-	/* cerr << dst(b.x2, b.y2, t.x1, t.y1) << "\n"; */
-	/* cerr << double(b.y2-t.y1) << "\n"; */
-	/* cerr << double(b.x2-t.x1) << "\n"; */
 	cout << dist << "\n";
 }
 
