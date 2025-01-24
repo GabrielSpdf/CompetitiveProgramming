ba604556e2550e9a85fbaf80c3767ee69c15fb30
GabrielSpdf <gb.spada@hotmail.com>
2025-01-23 22:33:16 -0300

    updating

---
+++ CodeForces/Rounds/Round887/a.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define FORI(sti, n) for(int i=(sti); i<(n); i++)
+#define FORJ(stj, n) for(int j=(stj); j<(n); j++)
+#define FORK(stk, n) for(int k=(stk); k<(n); k++)
+#define tvi(v) FORI(0, (v).size()) cerr << (v)[i] << " "; cerr << "\n";
+#define tii(val) cerr <<  "(" << (val).first << ", " << (val).second << ")\n";
+#define tvii(v) FORI(0, (v).size()) { tii((v)[i]) } cerr << "\n";
+#define tvvi(v) FORI(0, (v).size()) { FORJ(0, (v)[i].size()){ cerr <<  v[i][j] << " "; } cerr << "\n"; }  
+#define all(v) (v).begin(), (v).end() 
+#define rall(v) (v).rbegin(), (v).rend() 
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vbl;
+
+void solve(){
+	int n;
+	cin >> n; 
+	vi v(n);
+	FORI(0, n) cin >> v[i];
+	bool sorted=true;
+	FORI(1, n){
+		if(v[i]<v[i-1]) sorted=false;
+	}
+	
+	int ans=0;
+	if(sorted){
+		int mn=LLONG_MAX;
+		FORI(1, n){
+			if(v[i]-v[i-1]<mn) mn=v[i]-v[i-1];
+		}
+		ans = (mn+2)/2;
+	}
+
+
+	cout << ans << "\n";
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	
+	int t; cin >> t; while(t--)
+		solve();
+
+    return 0;
+}
---
+++ CodeForces/Rounds/Round887/c.cpp
@@ -0,0 +1,72 @@
+// Not solved yet
+
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define FORI(sti, n) for(int i=(sti); i<(n); i++)
+#define FORJ(stj, n) for(int j=(stj); j<(n); j++)
+#define FORK(stk, n) for(int k=(stk); k<(n); k++)
+#define tvi(v) FORI(0, (v).size()) cerr << (v)[i] << " "; cerr << "\n";
+#define tii(val) cerr <<  "(" << (val).first << ", " << (val).second << ")\n";
+#define tvii(v) FORI(0, (v).size()) { tii((v)[i]) } cerr << "\n";
+#define tvvi(v) FORI(0, (v).size()) { FORJ(0, (v)[i].size()){ cerr <<  v[i][j] << " "; } cerr << "\n"; }  
+#define all(v) (v).begin(), (v).end() 
+#define rall(v) (v).rbegin(), (v).rend() 
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vbl;
+
+void solve(){
+	int n, k;
+	cin >> n >> k;
+
+	map<int, ii> mp;
+	vi v(n);
+	FORI(0, n) cin >> v[i];
+	
+	mp[v[v.size()-1]] = make_pair(v[v.size()-1]+1, n);
+
+	for(int i=n-2; i>=0; --i){
+		if(mp.find(v[i]+1)==mp.end()){
+			mp[v[i]] = make_pair(v[i]+1, i+1);
+		}
+		else{
+			mp[v[i]] = mp[v[i+1]];
+		}
+	}
+
+	/* for(auto [a, b]: mp){ */
+	/* 	cerr << a << " " << b.first << " " << b.second; */ 
+	/* 	cerr << "\n"; */
+	/* } */
+	
+	int x=1, j=mp[x].second;
+	FORI(0, k){
+		while(mp.find(x+j)!=mp.end()){
+			auto it = --mp.upper_bound(x+j);
+			j = (*it).second.second;
+			/* cerr << "x+j: " << x+j << " | it.first: " << (*it).first << " | j: " << j << "\n"; */
+		}
+		x+=j;
+		/* cerr << "j: " << j << " "; */
+		/* cerr << x << " "; */
+	}
+	cout << x << "\n";
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	
+	int t; cin >> t; while(t--)
+		solve();
+
+    return 0;
+}
