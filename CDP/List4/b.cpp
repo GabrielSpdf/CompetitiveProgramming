820626ba0eacd8ae2d511d6c8d241b08565ca433
GabrielSpdf <gb.spada@hotmail.com>
2025-02-18 21:32:24 -0300

    updating

---
+++ CDP/List4/b.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define FORI(sti, n) for(int i=(sti); i<(n); i++)
+#define FORJ(stj, n) for(int j=(stj); j<(n); j++)
+#define FORK(stk, n) for(int k=(stk); k<(n); k++)
+#define tvi(v) FORI(0, (v).size()) cerr << (v)[i] << " "; cerr << "\n";
+#define tii(val) cerr <<  "(" << (val).first << ", " << (val).second << ")\n";
+#define tvii(v) FORI(0, (v).size()) { tii((v)[i]) } cerr << "\n";
+#define tvvi(v) FORI(0, (v).size()) { FORJ(0, (v)[i].size()){ cerr <<  v[i][j] << " "; } cerr << "\n"; }  
+#define all(v) (v).begin(), (v).end() 
+#define rall(v) (v).rbegin(), (v).rend() 
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vbl;
+
+void solve(){
+  int len;
+  string s;
+  cin >> len >> s;
+
+  vi freq(26);
+  FORI(0, len) freq[s[i]-0x61]+=1;
+
+  pair<pair<int, char>, int> lo, hi;
+
+  lo = hi = {{freq[s[0]-0x61], s[0]}, 0};
+
+  FORI(1, len){
+    lo = min(lo, {{freq[s[i] - 0x61], s[i]}, i});
+    hi = max(hi, {{freq[s[i] - 0x61], s[i]}, i});
+  }
+  s[lo.second] = s[hi.second];
+  cout << s << "\n";
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	
+	int t; cin >> t; while(t--)
+		solve();
+
+    return 0;
+}
