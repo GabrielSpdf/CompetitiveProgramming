9484a9802fc12ac85b9ba2043e23a083855e5f5a
GabrielSpdf <gb.spada@hotmail.com>
2025-04-09 01:04:38 -0300

    weekly cdp

---
+++ CDP/List6/Regular/e.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define FORI(sti, n) for(int i=(sti); i<(n); i++)
+#define FORJ(stj, n) for(int j=(stj); j<(n); j++)
+#define FORK(stk, n) for(int k=(stk); k<(n); k++)
+#define tvi(v) FORI(0, (v).size()) cerr << (v)[i] << " "; cerr << "\n";
+#define tii(val) cerr <<  "(" << (val).first << ", " << (val).second << ")\n";
+#define tvii(v) FORI(0, (v).size()) { tii((v)[i]) } cerr << "\n";
+#define tvvi(v) FORI(0, (v).size()) { FORJ(0, (v)[i].size()){ cerr <<  v[i][j] << " "; } cerr << "\n"; }  
+#define all(v) (v).begin(), (v).end() 
+#define rall(v) (v).rbegin(), (v).rend() 
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<bool> vbl;
+
+void solve(){
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    FORI(0, (int)s.size()){
+        mp[s[i]]++;
+    }
+
+    string ans="", aux="";
+    for(auto [ch, freq] : mp){
+        FORI(0, freq){
+            ans+=ch;
+            aux+=ch;
+        }
+    }
+    reverse(all(aux));
+
+    cout << ans+aux << "\n";
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	
+	int t; cin >> t; while(t--)
+		solve();
+
+    return 0;
+}
